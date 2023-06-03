e54178a583ceed67d184be401e2f381b737bd55d
Sangeet Sharma <sangeetsharmaofficial@gmail.com>
2023-06-03 13:56:04 +0530

    added js advanced concepts like iterators, iterables and generators

--- chapter9_advancedJavascript/_12_iterators_and_iterables.js
+++ chapter9_advancedJavascript/_12_iterators_and_iterables.js
@@ -6,8 +6,58 @@ In JavaScript, iterators and iterables are mechanisms that allow you to traverse
 
 Here's an overview of iterators and iterables:
 
-Iterables: An iterable is an object that defines an iterator, which is responsible for providing a sequence of values. Any object that implements the Symbol.iterator method is considered iterable. Iterables include arrays, strings, maps, sets, and other built-in JavaScript data structures.
+Iterables: An  iterable is an object that defines an iterator, which is responsible for providing a sequence of values. Any object that implements the Symbol.iterator method is considered iterable. Iterables include arrays, strings, maps, sets, and other built-in JavaScript data structures.
 
 Iterator: An iterator is an object that provides a way to access elements sequentially from an iterable object. It must implement a next() method, which returns an object with two properties: value (the current value) and done (a boolean indicating if there are more values to iterate over). When all elements have been iterated, done is set to true.
 
+in the normal iteration in javascript eg- normal for loop
+
+1. There is a difficulty in accessing the element
+
+2. Difficult to manage iteration on the data for various types of data structures
+
+
+There was a need to iterate over various data structures in a new way that abstracts away the complexity of accessing elements one by one and was at the same time uniform across the different data structures.
+
+
+Iterables and iterators help us to work upon every single data and allow us to operate upon that data rather than to think about how to access that data.
+
+An object which implements the iterable protocol is called an iterable.
+
+Iterable Protocol - For an objecct to be an iterable it must implement a method at the key. [Symbol.iterator]
+
+That method should not accept any argument and should return an object which conforms to the iterator protocol.
+
+The iterator protocol decides whether an object is an iterator.
+
+The object must have a next() method that returns an object with two properties 
+
+1. value -> which gives the current element.
+2. done -> which is a boolean value indicating whether or not there are any more elements that could be iterated upon.
+
+
 */
+
+const obj = {
+    [Symbol.iterator]: function(){
+        let step = 0
+
+        const iterator = {
+            next: function(){
+                step++
+                if(step == 1){
+                    return {value: 'Hellloo', done: false}
+                }else if(step == 2){
+                    return {value:  'World', done: false}
+                }
+                return {value: undefined, done:true}
+            }
+        }
+        return iterator 
+    }
+}
+
+
+for (const word of obj){
+    console.log(word);
+}
---
+++ chapter9_advancedJavascript/_13_generators.js
@@ -0,0 +1,42 @@
+// ----------- generators in javascript --------------
+
+/*
+in JavsScript, generators are functions that can be paused and resumed, allowing for the generation of a sequence of values. They are defined using function* syntax and can use the `yield` keyword to produce a value and suspend execution
+*/
+
+function* numberGenerator(){
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+// create an instance of the generator
+const generator = numberGenerator();
+
+// calling the generator function returns an iterator
+// we can use the iterator's next() method to retrieve the next value 
+console.log(generator.next().value); //output: 1
+console.log(generator.next().value); // Output: 2
+console.log(generator.next().value); //Output: undefined
+
+/*
+In the above example the numberGenerator function is a generator that yields the values 1, 2 and 3. Each time we call the next() method on the generator object
+*/
+
+//Generators can also receive values from outside using the yield keyword 
+function* wordGenerator(){
+    const x = yield "First value";
+  const y = yield "Second value";
+  yield x + y;
+}
+
+const generatorObj = wordGenerator();
+console.log(generatorObj.next().value); // Output: "First value"
+console.log(generatorObj.next(2).value); // Output: "Second value"
+console.log(generatorObj.next(4).value); // Output: 6
+
+//In this case, when we call generator.next(2), the value 2 is passed as the result of the previous yield expression. Similarly, when we call generator.next(4), the value 4 is assigned to the variable y and used in the final yield expression.
+
+
+
+
