ca8f1a8645a8b92e7fb08d415737934d7612a838
Sangeet Sharma <sangeetsharmaofficial@gmail.com>
2023-07-18 19:22:10 +0530

    learnt about comparison operators

---
+++ chapter1 starters/comparisonOperators.js
@@ -0,0 +1,18 @@
+// equality operators
+
+/*
+1. equality (==)
+    it only compares the value
+
+2. inequality (!=)
+
+    this also compares value
+    eg -> (5 != '5')  returns false
+
+3. strict -> (===), (!==)  -> takes care of the data type as well
+
+
+*/
+
+
+// comparison operators -> comparison operators contains basic operators like >,<,>=,<= etc.
