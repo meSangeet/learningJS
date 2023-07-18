d472272f0194742c4a77fbc50a9f2080a8fe00ee
Sangeet Sharma <sangeetsharmaofficial@gmail.com>
2023-07-18 21:16:13 +0530

    push pop slice method:

---
+++ chapter8_Arrays/_04_methods1.js
@@ -0,0 +1,20 @@
+// Array push() method
+
+// this method can contain as many number of parameters as the number of elements to be inserted into the array. 
+//Return value -> this method returns the new length array after inserting the arguments into the array.
+
+
+// Array pop()  method
+
+//arr.pop() removes the last element of the array and also returns the removed element. This function decreases the length of the array.
+
+
+
+// Array slice() method     
+
+// arr.slice(begin, end) -> returns a new array containing a portion of the array on which it is implemented. The original array remains unchanged.
+
+
+//begin -> defines the starting point from where the portion is to be extracted. if this argument is missing then the method takes begin as 0.
+
+//end -> this parameter  is the index upto which the portion is extracted (excluding the end index). if this argument is not defined then the array till the end is extracted.
