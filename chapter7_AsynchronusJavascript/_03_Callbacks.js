68b77447fdab8e85ea07ac153c7e2de494c8ca53
Sangeet Sharma <sangeetsharmaofficial@gmail.com>
2023-07-19 00:29:58 +0530

    callback and callback hell

--- chapter7_AsynchronusJavascript/_03_Callbacks.js
+++ chapter7_AsynchronusJavascript/_03_Callbacks.js
@@ -31,3 +31,33 @@ resultCalculator(3,4,resultDisplay);
 
 // Asynchronous programmes are difficult to write and difficult to debug. Because of this, most modern asynchronous JavaScript methods don't use callbacks. Instead, in JavaScript, asynchronous programming is solved using promises instead.
 
+
+
+// callback hell
+
+/*
+
+Callback hell, also known as the pyramid of doom, is a term used in asynchronous programming to describe a situation where callbacks are nested within callbacks, resulting in deeply indented and complex code. It occurs when working with functions or tasks that depend on the completion of other asynchronous operations.
+
+In languages that use callback-style asynchronous programming, such as JavaScript, callbacks are functions that are passed as arguments to other functions. These callbacks are executed when an asynchronous operation, such as reading from a file or making an API call, completes.
+
+When multiple asynchronous operations need to be executed sequentially or dependent on each other, developers often end up nesting these callbacks. Each callback contains the code that should be executed after the completion of the previous asynchronous operation. As more operations are added, the indentation level of the code increases, resulting in an unreadable and difficult-to-maintain code structure.
+
+Here's an example of callback hell in JavaScript:
+*/
+
+asyncOperation1(arg1, function(result1) {
+    asyncOperation2(arg2, function(result2) {
+        asyncOperation3(arg3, function(result3) {
+            // More nested callbacks...
+        });
+    });
+});
+
+/*
+As you can see, the code becomes increasingly nested and harder to follow as more operations are added. This can make the code difficult to understand, debug, and maintain. It also makes error handling and control flow more complex.
+
+To mitigate callback hell, several techniques have been developed over the years. One approach is to use named functions instead of anonymous callbacks, which can improve code readability. Another solution is to use control flow libraries or frameworks that provide more structured and readable ways to handle asynchronous operations, such as Promises or async/await in JavaScript.
+
+By adopting these techniques, developers can write asynchronous code that is more readable, modular, and maintainable, avoiding the pitfalls of callback hell.
+*/
